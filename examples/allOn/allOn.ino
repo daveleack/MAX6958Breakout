f3d4d1a998c444d1b4c6bc56c9882019ae3b81c1
daveleack <daveleack@gmail.com>
2018-04-06 23:41:49 +0100

    Update allOn.ino

--- examples/allOn/allOn.ino
+++ examples/allOn/allOn.ino
@@ -6,7 +6,7 @@ void setup() {
   //    init() - Must be called first. Sets up Wire library and brings chip out of sleep mode
   //    clear() - Clears out all data, turning all LEDs off
   //    setChannel(uint8_t LEDAddr, bool On)  - Sets the LED at address LEDAddr (1 - 36, or 255 for all) to value On (true/false). Values of LEDAddr at 0 or higher than 36 will be ignored and return code OUTOFRANGE (which is 0x05), will be sent back.
-  //    setBrightness(uint8_t bri) - Sets the global brightness of all LED channels to bri (0 to 63, higher values will be clamped to 63)
+  //    setBrightness(uint8_t Bri) - Sets the global brightness of all LED channels to Bri (0 to 63, higher values will be clamped to 63)
   
   leds.init();
   byte returnCode = leds.setChannel(255, true); //Set all LED channels to ON
